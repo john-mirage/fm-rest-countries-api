0f8b65e52df1b949294d7a20163d34958294c60a
john-mirage <chaosdynamix@posteo.net>
2022-06-19 23:49:33 +0000

    feat: add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@styles/global.css";
 import Head from "next/head";
 import Layout from "@components/layout";
+import favicon from "@images/favicon.png";
 
 function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <link rel="icon" type="image/png" href={favicon.src}/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Layout>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 function Document() {
   return (
     <Html lang="en" dir="ltr" data-theme="light">
       <Head>
         <meta charSet="UTF-8" />
-        <link rel="icon" type="image/png" href="/src/assets/images/favicon.png"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700&display=swap" rel="stylesheet"/>
