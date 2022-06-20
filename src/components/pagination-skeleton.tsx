645ffde75a1b5b11f16988888b6c9196c52bf440
john-mirage <chaosdynamix@posteo.net>
2022-06-20 01:20:59 +0000

    ref: improve pagination skeleton text

--- src/components/pagination-skeleton.tsx
+++ src/components/pagination-skeleton.tsx
@@ -1,6 +1,6 @@
 function PaginationSkeleton() {
   return (
-    <div className="lg:flex lg:flex-row lg:justify-between lg:items-center">
+    <div className="lg:flex lg:flex-row lg:justify-between lg:items-start">
       <div className="animate-pulse mb-16 lg:mb-0 lg:flex-none w-200 h-14 rounded-full bg-light-element dark:bg-dark-element"></div>
       <div className="space-x-16 flex flex-row justify-start items-center lg:flex-none">
         <div className="flex w-96 h-40 rounded-2 shadow bg-light-surface dark:bg-dark-surface">
