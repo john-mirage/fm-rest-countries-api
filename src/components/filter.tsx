494ee363e70c24990b9e394e9c07bddc3d58ddfd
john-mirage <chaosdynamix@posteo.net>
2022-06-20 21:29:44 +0000

    feat: add filter

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import { Listbox } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/outline";
+
 const regions: string[] = [
+  "all",
   "africa",
   "america",
   "asia",
   "europe",
   "oceania"
 ];
+
+function Filter() {
+  const [selectedRegion, setSelectedRegion] = useState(regions[0]);
+
+  return (
+    <Listbox
+      className="relative w-200 h-48 rounded-6 bg-light-surface shadow dark:bg-dark-surface"
+      value={selectedRegion}
+      onChange={setSelectedRegion}
+      as="div"
+    >
+      <Listbox.Button className="flex flex-row justify-between items-center px-24 w-full h-full text-light-text dark:text-dark-text">
+        <span className="text-12 font-400 capitalize">{selectedRegion === "all" ? "Filter by region" : selectedRegion}</span>
+        <ChevronDownIcon className="w-12 h-12" />
+      </Listbox.Button>
+      <Listbox.Options className="absolute -bottom-4 left-0 translate-y-full w-full max-h-400 overflow-auto py-10 shadow rounded-6 bg-light-surface dark:bg-dark-surface">
+        {regions.map((region) => (
+          <Listbox.Option
+            className="px-24 py-8 text-12 font-400 text-light-text dark:text-dark-text capitalize"
+            key={region}
+            value={region}
+          >
+            { region }
+          </Listbox.Option>
+        ))}
+      </Listbox.Options>
+    </Listbox>
+  );
+}
+
+export default Filter;
--- src/components/tool-bar.tsx
+++ src/components/tool-bar.tsx
@@ -1,9 +1,11 @@
-import SearchBar from "./search-bar";
+import Filter from "@components/filter";
+import SearchBar from "@components/search-bar";
 
 function ToolBar({ countries }) {
   return (
     <div className="mb-32 lg:flex lg:flex-row lg:justify-between lg:items-center">
       <SearchBar countries={countries} />
+      <Filter />
     </div>
   );
 }
