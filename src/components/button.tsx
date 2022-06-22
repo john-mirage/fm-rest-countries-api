1929dfa659c32c970fce341ca2322568edaa4e0d
john-mirage <chaosdynamix@posteo.net>
2022-06-22 01:54:08 +0000

    fix: fix country fetching

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,7 +8,7 @@ function Button({ borderCountry }) {
   }
 
   return (
-    <button onClick={handleClick} className="mx-5 mb-10 flex w-96 h-28 rounded-2 shadow bg-light-surface dark:bg-dark-surface">
+    <button onClick={handleClick} className="mx-5 mb-10 px-16 flex min-w-96 h-28 rounded-2 shadow bg-light-surface dark:bg-dark-surface">
       <span className="m-auto text-12 font-300 text-light-text dark:text-dark-text">{borderCountry.name}</span>
     </button>
   );
--- src/hooks/use-country.tsx
+++ src/hooks/use-country.tsx
@@ -3,7 +3,7 @@ import useSWR from "swr";
 const fetcher = (url: string) => fetch(url).then(res => res.json());
 
 function useCountry(countryCode: string) {
-  const { data, error } = useSWR(`https://restcountries.com/v2/alpha/${countryCode}?fields=name,nativeName,population,region,subregion,capital,topLevelDomain,currencies,languages,borders,flag`, fetcher);
+  const { data, error } = useSWR(countryCode ? `https://restcountries.com/v2/alpha/${countryCode}?fields=name,nativeName,population,region,subregion,capital,topLevelDomain,currencies,languages,borders,flags` : null, fetcher);
 
   return {
     country: data,
