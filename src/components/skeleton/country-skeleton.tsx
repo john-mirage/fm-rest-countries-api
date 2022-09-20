c75cf51c8a8a94372928d0c6f77ca1f239ca4cd9
john-mirage <chaosdynamix@posteo.net>
2022-09-20 16:57:26 +0200

    fix: fix country skeleton

--- src/components/skeleton/country-skeleton.tsx
+++ src/components/skeleton/country-skeleton.tsx
@@ -1,9 +1,11 @@
 function CountrySkeleton() {
   return (
-    <div className="pt-64 lg:flex lg:flex-row lg:justify-start lg:items-center">
-      <div className="animate-pulse mb-48 w-full h-230 bg-light-element dark:bg-dark-element lg:mb-0 lg:flex-none lg:mr-80 lg:w-560 lg:h-400"></div>
+    <div className="md:w-568 md:mx-auto lg:w-full lg:mx-0 lg:flex lg:flex-row lg:justify-start lg:items-center">
+      <div className="animate-pulse mb-48 w-full lg:mb-0 lg:flex-none lg:mr-80 lg:w-560">
+        <div className="w-full pt-60% bg-light-element dark:bg-dark-element"></div>
+      </div>
       <div className="lg:flex-1 lg:ml-40">
-        <div className="animate-pulse mb-28 w-full h-22 rounded-full bg-light-element dark:bg-dark-element"></div>
+        <div className="animate-pulse mb-28 w-200 h-22 rounded-full bg-light-element dark:bg-dark-element"></div>
         <div className="animate-pulse lg:flex lg:flex-row lg:items-start">
           <div className="mb-48 lg:flex-1 lg:mr-16">
             <div className="mb-18 w-full h-14 rounded-full bg-light-element dark:bg-dark-element"></div>
