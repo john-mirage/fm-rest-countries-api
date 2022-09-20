4bcf1b00c72076b272673fd1cbdb8b8d6a0859d4
john-mirage <chaosdynamix@posteo.net>
2022-09-20 16:46:20 +0200

    fix: fix toolbar skeletons margins

--- src/components/skeleton/tool-bar-skeleton.tsx
+++ src/components/skeleton/tool-bar-skeleton.tsx
@@ -3,7 +3,7 @@ import FilterSkeleton from "@components/skeleton/filter-skeleton";
 
 function ToolbarSkeleton() {
   return (
-    <div className="mb-32 lg:flex lg:flex-row lg:justify-between lg:items-center">
+    <div className="mt-24 mb-32 md:mt-48 md:mb-48 lg:flex lg:flex-row lg:justify-between lg:items-center">
       <SearchBarSkeleton />
       <FilterSkeleton />
     </div>
